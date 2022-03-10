89f9522ebedd8375e95c84ae6685e6968a93df75
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-03-10 13:22:09 +0530

    add createUser function - emit UserCreated event

---
+++ contracts/Aegis.sol
@@ -0,0 +1,42 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Aegis {
+    address public manager;
+
+    struct User {
+        string username;
+        address payable publicKey;
+    }
+
+    event userCreated(string username, address payable publicKey);
+
+    // User [] public users;
+    mapping(address => User) public users;
+
+    constructor() {
+        manager = msg.sender;
+    }
+
+    //checking if an user already exists
+    modifier isNotAnExistingUser() {
+        require(
+            users[msg.sender].publicKey == address(0),
+            "User already exists."
+        );
+        _;
+    }
+
+    function createAccount(string memory username) public isNotAnExistingUser {
+        User memory newUser = User({
+            username: username,
+            publicKey: payable(msg.sender)
+        });
+        users[msg.sender] = newUser;
+
+        //emit new event
+        emit userCreated({username: username, publicKey: payable(msg.sender)});
+    }
+}
