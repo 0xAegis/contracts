71fb2fe4080a9dacf09a7049aedb9d48b70517af
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-03-10 15:05:04 +0530

    add new test - fails to create a new user when user already exists

--- test/test.js
+++ test/test.js
@@ -24,5 +24,23 @@ describe("Aegis", function () {
       expect(user.username).to.equal("sample username");
       expect(user.publicKey).to.equal(addr1.address);
     });
+
+    it("fails to create a new user when user already exists", async function () {
+      try {
+        //creating a new user first time
+        const newUserTx1 = await aegis.createAccount("sample username 1");
+        await newUserTx1.wait();
+
+        //creating a new user second time from same address as before
+        const newUserTx2 = await aegis.createAccount("sample username 2");
+      } catch (error) {
+        expect(error.message).to.include("User already exists.");
+        return;
+      }
+      //If no error is thrown in the try block
+      expect.fail(
+        "Did not fail to create a new user while user already exists"
+      );
+    });
   });
 });
