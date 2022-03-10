232b4caefffb16d2f748de9747a2d9f0687b1ae8
Sumit Ghosh <sumit@sumit-ghosh.com>
2022-03-10 21:49:05 +0530

    Fix a type and remove an unneeded try catch block

--- test/test.js
+++ test/test.js
@@ -2,24 +2,20 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Aegis", function () {
-  describe("User Acount", function () {
+  describe("User Account", function () {
     let aegis;
     let addr1, addr2;
 
     beforeEach(async () => {
       [addr1, addr2] = await ethers.getSigners();
-      const Aegis = await ethers.getContractFactory("Aegis");
-      aegis = await Aegis.deploy();
+      const aegisFactory = await ethers.getContractFactory("Aegis");
+      aegis = await aegisFactory.deploy();
       await aegis.deployed();
     });
 
     it("can create a new user", async function () {
-      try {
-        const newUserTx = await aegis.createUser("sample username");
-        await newUserTx.wait();
-      } catch (error) {
-        console.log(error.message);
-      }
+      const newUserTx = await aegis.createUser("sample username");
+      await newUserTx.wait();
       const user = await aegis.users(addr1.address);
       expect(user.username).to.equal("sample username");
       expect(user.publicKey).to.equal(addr1.address);
