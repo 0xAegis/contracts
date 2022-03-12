b93a7854e2365141e6c9b6fa74c07504dd930aab
Sumit Ghosh <sumit@sumit-ghosh.com>
2022-03-12 16:11:13 +0530

    feat: associate an NFT collection with each user (#3)
    * Add NFT contract generated using OpenZeppelin wizard
    
    * add followUser, mint NFTs for each follower
    
    * Add tests
    
    * rename tokenAddress to nftAddress
    
    * Remove followUser function for now

--- contracts/Aegis.sol
+++ contracts/Aegis.sol
@@ -1,17 +1,32 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+
+interface AegisFollowers is IERC721Enumerable {
+    function owner() external view returns (address);
+
+    function safeMint(address to) external;
+}
 
 contract Aegis {
+    using Counters for Counters.Counter;
+
     address public manager;
 
     struct User {
         string username;
         address payable publicKey;
+        address nftAddress;
     }
 
-    event UserCreated(string username, address payable publicKey);
+    event UserCreated(
+        string username,
+        address payable publicKey,
+        address nftAddress
+    );
+    event UserFollowed(address follower, address followed);
 
     // User [] public users;
     mapping(address => User) public users;
@@ -20,23 +35,55 @@ contract Aegis {
         manager = msg.sender;
     }
 
-    //checking if an user already exists
-    modifier isNotAnExistingUser() {
+    //checks if the caller is an user
+    modifier callerIsUser() {
         require(
-            users[msg.sender].publicKey == address(0),
-            "User already exists."
+            users[msg.sender].publicKey != address(0),
+            "User does not exist."
         );
         _;
     }
 
-    function createUser(string memory username) public isNotAnExistingUser {
+    //checks if the public key is an user
+    modifier isUser(address publicKey) {
+        require(
+            users[publicKey].publicKey != address(0),
+            "User does not exist."
+        );
+        _;
+    }
+
+    function createUser(string memory username, address nftAddress) public {
+        AegisFollowers token = AegisFollowers(nftAddress);
+
+        //user should not already exist
+        require(
+            users[msg.sender].publicKey == address(0),
+            "User already exists."
+        );
+        //the owner of the collection should be this contract, so that only it can mint new NFTs
+        require(
+            token.owner() == address(this),
+            "NFT collection is not owned by Aegis."
+        );
+        //the NFT collection should be new, with zero existing minted NFTs
+        require(
+            token.totalSupply() == 0,
+            "NFT collection's total supply is not zero."
+        );
+
         User memory newUser = User({
             username: username,
-            publicKey: payable(msg.sender)
+            publicKey: payable(msg.sender),
+            nftAddress: nftAddress
         });
         users[msg.sender] = newUser;
 
         //emit new event
-        emit UserCreated({username: username, publicKey: payable(msg.sender)});
+        emit UserCreated({
+            username: username,
+            publicKey: payable(msg.sender),
+            nftAddress: nftAddress
+        });
     }
 }
--- test/test.js
+++ test/test.js
@@ -3,39 +3,129 @@ const { ethers } = require("hardhat");
 
 describe("Aegis", function () {
   describe("User Account", function () {
-    let aegis;
+    let aegis, aegisFollowersFactory;
     let addr1, addr2;
 
     beforeEach(async () => {
       [addr1, addr2] = await ethers.getSigners();
+      aegisFollowersFactory = await ethers.getContractFactory(
+        "contracts/AegisFollowers.sol:AegisFollowers"
+      );
       const aegisFactory = await ethers.getContractFactory("Aegis");
       aegis = await aegisFactory.deploy();
       await aegis.deployed();
     });
 
     it("can create a new user", async function () {
-      const newUserTx = await aegis.createUser("sample username");
+      // deploy an AegisFollowers NFT collection
+      const aegisFollowers = await aegisFollowersFactory.deploy();
+      await aegisFollowers.deployed();
+      //transfer ownership to Aegis
+      const transferOwnershipTx = await aegisFollowers.transferOwnership(
+        aegis.address
+      );
+      await transferOwnershipTx.wait();
+
+      const newUserTx = await aegis.createUser(
+        "sample username",
+        aegisFollowers.address
+      );
       await newUserTx.wait();
+
       const user = await aegis.users(addr1.address);
       expect(user.username).to.equal("sample username");
       expect(user.publicKey).to.equal(addr1.address);
+      expect(user.nftAddress).to.equal(aegisFollowers.address);
     });
 
     it("fails to create a new user when user already exists", async function () {
+      // deploy an AegisFollowers NFT collection and transfer ownership to Aegis
+      const aegisFollowers = await aegisFollowersFactory.deploy();
+      await aegisFollowers.deployed();
+      const transferOwnershipTx = await aegisFollowers.transferOwnership(
+        aegis.address
+      );
+      await transferOwnershipTx.wait();
+
       try {
         //creating a new user first time
-        const newUserTx1 = await aegis.createUser("sample username 1");
+        const newUserTx1 = await aegis.createUser(
+          "sample username 1",
+          aegisFollowers.address
+        );
         await newUserTx1.wait();
 
         //creating a new user second time from same address as before
-        const newUserTx2 = await aegis.createUser("sample username 2");
+        const newUserTx2 = await aegis.createUser(
+          "sample username 2",
+          aegisFollowers.address
+        );
       } catch (error) {
         expect(error.message).to.include("User already exists.");
         return;
       }
+
       //If no error is thrown in the try block
       expect.fail(
         "Did not fail to create a new user while user already exists"
+      );
+    });
+
+    it("fails to create a new user when NFT collection is not owned by the Aegis contract", async function () {
+      // deploy an AegisFollowers NFT collection
+      const aegisFollowers = await aegisFollowersFactory.deploy();
+      await aegisFollowers.deployed();
+
+      try {
+        //creating a new user first time
+        const newUserTx = await aegis.createUser(
+          "sample username",
+          aegisFollowers.address
+        );
+        await newUserTx1.wait();
+      } catch (error) {
+        expect(error.message).to.include(
+          "NFT collection is not owned by Aegis."
+        );
+        return;
+      }
+
+      //If no error is thrown in the try block
+      expect.fail(
+        "Did not fail to create a new user when token contract is not owned by Aegis."
+      );
+    });
+
+    it("fails to create a new user when there are existing minted NFTs for the given collection", async function () {
+      // deploy an AegisFollowers NFT collection
+      const aegisFollowers = await aegisFollowersFactory.deploy();
+      await aegisFollowers.deployed();
+      //mint an NFT
+      const mintNftTx = await aegisFollowers.safeMint(addr2.address);
+      await mintNftTx.wait();
+      //transfer ownership to Aegis
+      const transferOwnershipTx = await aegisFollowers.transferOwnership(
+        aegis.address
+      );
+      await transferOwnershipTx.wait();
+
+      try {
+        //creating a new user first time
+        const newUserTx = await aegis.createUser(
+          "sample username",
+          aegisFollowers.address
+        );
+        await newUserTx1.wait();
+      } catch (error) {
+        expect(error.message).to.include(
+          "NFT collection's total supply is not zero."
+        );
+        return;
+      }
+
+      //If no error is thrown in the try block
+      expect.fail(
+        "Did not fail to create a new user when there are existing minted NFTs for the given collection"
       );
     });
   });
